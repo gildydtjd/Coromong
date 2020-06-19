9475901a392ab567dfeea0c0a0b25f34fe45977f
dragon <gildydtjd@naver.com>
2020-06-19 23:06:40 +0900

    공지사항, 뉴스

--- src/body/News.js
+++ src/body/News.js
@@ -4,7 +4,26 @@ export default class News extends Component {
   render() {
     return (
       <div className="news">
+        <div className="news_box">
+        <img className="newsImage1" src="./Img/corona1.jpg"></img><br></br>
+        <a className="news_font" href="https://www.yna.co.kr/view/AKR20200619047851063?input=1195m"> 코로나19 대전 확산 지속…확진자 근무 요양병원 환자 전수검사</a>
+        </div>
         
+        <div className="news_box">
+        <img className="newsImage1" src="./Img/corona2.jpg"></img><br></br>
+        <a className="news_font" href="http://www.wowtv.co.kr/NewsCenter/News/Read?articleId=A202006190245&t=NN"> 제넥신 “코로나19 예방 백신, 첫 투여 완료”</a>
+        </div>
+
+        <br></br>
+        <div className="news_box">
+        <img className="newsImage1" src="./Img/corona3.jpg"></img><br></br>
+        <a className="news_font" href="https://www.hankyung.com/international/article/2020061962827"> WHO "올해 말 코로나 백신 1~2종 개발 가능성 있어"</a>
+        </div>
+
+        <div className="news_box">
+        <img className="newsImage1" src="./Img/corona4.jpg"></img><br></br>
+        <a className="news_font" href="https://news.chosun.com/site/data/html_dir/2020/06/19/2020061900355.html?utm_source=naver&utm_medium=original&utm_campaign=news"> 트럼프 중국이 미국 경제 망치려 코로나 퍼트렸을 수도"</a>
+        </div>
       </div>
     )
   }
--- src/body/Notice.js
+++ src/body/Notice.js
@@ -10,38 +10,38 @@ export default class Notice extends Component {
         <th scope="cols">Count</th>
         <th scope="cols">Title</th>
         <th scope="cols">Content</th>
-        <th scope="cols">Content</th>
+        <th scope="cols">Views</th>
         </tr>
         </thead>
 
         <tbody>
 
         <tr>
-        <th scope="row">항목명</th>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
+        <th scope="row">1</th>
+        <td>코로나19 수도권 방역 관리 방안</td>
+        <td>2020.06.19(금) 코로나 수도권...</td>
+        <td>128</td>
         </tr>
 
         <tr>
-        <th scope="row">항목명</th>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
+        <th scope="row">2</th>
+        <td>다중이용시설 이용 코로나19 검사</td>
+        <td>2020.05.20(수) 다중이용시설...</td>
+        <td>37</td>
         </tr>
 
         <tr>
-        <th scope="row">항목명</th>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
+        <th scope="row">3</th>
+        <td>이태원 클럽 방문 코로나19 검사</td>
+        <td>2020.05.28(금) 이태원 클럽...</td>
+        <td>76</td>
         </tr>
 
         <tr>
-        <th scope="row">항목명</th>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
-        <td>내용이 들어갑니다.</td>
+        <th scope="row">4</th>
+        <td>코로나19 위기극복 성금 전달</td>
+        <td>2020.06.15(화) 위기극복성금...</td>
+        <td>112</td>
         </tr>
         
         </tbody>
