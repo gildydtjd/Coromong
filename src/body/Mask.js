7b937cb79511d8fe33ed869c974bb936ba526d24
dragon <gildydtjd@naver.com>
2020-07-04 05:02:22 +0900

    mask 페이지

--- src/body/Mask.js
+++ src/body/Mask.js
@@ -1,39 +1,52 @@
 import React, { Component } from 'react'
-import Axios from 'axios';
 import './mask.css'
+import { Link } from 'react-router-dom';
+
 
 export default class Mask extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      address: "",
-      count: "",
-      stores: [{
-        addr: "",
-        code: "",
-        created_at: "",
-        lat: "",
-        lng: "",
-        name: "",
-        remain_stat: "",
-        stock_at: "",
-        type: ""
-      }]
+      name : "",
+      phone : ""
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
-  componentDidMount(){
-    Axios.get("https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json").then(res => {
-      const state = res.data;
-      console.log(res.data);
-      this.setState(state);
-    })
-  }
-  
+        handleChange = (e) => {
+          this.setState({
+            [e.target.name]: e.target.value
+          })
+        }
+    
+         handleSubmit = (e) => {
+          e.preventDefault();
+          this.setState({
+            name: '',
+            phone: ''
+          })
+        }
   render() {
+
      return (
       <div className="mask">
-        </div>
-    )
+        <div style={{width : '20%'}}>
+        <img src="./Img/mask.PNG" style={{width : '60%'}}></img>
+        <h1 className="mask_title">마스크 판매처 조회</h1>
+        <label className="form-label" for="formBasicEmail">판매처를 찾을 주소를 입력해 주세요</label>
+        <form onSubmit={this.handleSubmit} className="search_form">
+        <input
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+          class="form-control"
+        />
+        <Link to={"/Mask/" + this.state.name}><button type="submit" className="btn-primary">주소 조회</button></Link>
+      </form>
+      </div>
+      </div>
+     )
   }
 }
